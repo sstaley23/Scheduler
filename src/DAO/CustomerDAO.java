94d194d0c6cae21d3d9f5792713c982400ba83d6
sstaley23 <sstaley23@gmail.com>
2023-06-16 09:09:30 -0600

    Created an update method

--- src/DAO/CustomerDAO.java
+++ src/DAO/CustomerDAO.java
@@ -59,6 +59,29 @@ public abstract class CustomerDAO {
         return rowsAffected;
     }
 
+    /** Updates customer in database
+     * @param id
+     * @param name
+     * @param address
+     * @param postalCode
+     * @param phone
+     * @param divisionID
+     * @return
+     * @throws SQLException
+     */
+    public static  int upDateCustomer(int id, String name, String address, String postalCode, String phone, int divisionID) throws SQLException {
+        String sql = "UPDATE customers SET Customer_Name = ?, Address = ?, Postal_Code = ?, Phone = ?, Division_ID = ? WHERE Customer_ID = ?";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ps.setString(1, name);
+        ps.setString(2, address);
+        ps.setString(3, postalCode);
+        ps.setString(4, phone);
+        ps.setInt(5, divisionID);
+        ps.setInt(6, id);
+        int rowsAffected = ps.executeUpdate();
+        return rowsAffected;
+    }
+
     /** Removes customer from database
      * @param customerID
      * @return
