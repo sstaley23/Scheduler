899129248323cf883c608ede63ae90c72bfd470a
sstaley23 <sstaley23@gmail.com>
2023-06-22 11:32:13 -0600

    Created checkOverlap method

--- src/Utilities/TimeManager.java
+++ src/Utilities/TimeManager.java
@@ -1,8 +1,11 @@
 package Utilities;
 
+import DB.AppointmentsDB;
+import Model.Appointments;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.sql.SQLException;
 import java.time.*;
 import java.util.List;
 
@@ -68,5 +71,36 @@ public class TimeManager {
         LocalDateTime utcLDT = utcZDT.toLocalDateTime();
 
         return utcLDT;
+    }
+
+    /** Checks for appointment overlaps and returns true if found
+     * @param start
+     * @param end
+     * @param id
+     * @return
+     * @throws SQLException
+     */
+    public static boolean checkOverlap(LocalDateTime start, LocalDateTime end, int id) throws SQLException {
+        boolean overlap = false;
+        ObservableList<Appointments> appts = AppointmentsDB.getCustomerAppointments(id);
+
+        for(Appointments appt : appts){
+            //Scenario 1
+            if((start.isAfter(appt.getStartDateTime()) || start.isEqual(appt.getStartDateTime())) && start.isBefore(appt.getEndDateTime())) {
+                System.out.println("Scenario 1 triggered");
+                overlap = true;
+            }
+            //Scenario 2
+            if((end.isBefore(appt.getEndDateTime()) || end.isEqual(appt.getEndDateTime())) && end.isAfter(appt.getStartDateTime())){
+                System.out.println("Scenario 2 triggered");
+                overlap = true;
+            }
+            //Scenario 3
+            if((start.isBefore(appt.getStartDateTime()) || start.isEqual(appt.getStartDateTime())) && (end.isAfter(appt.getEndDateTime()) || end.isEqual(appt.getEndDateTime()))) {
+                System.out.println("Scenario 3 triggered");
+                overlap = true;
+            }
+        }
+        return overlap;
     }
 }
