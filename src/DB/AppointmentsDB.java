c728809d6ff92ce19147af1b3e0072c892ea44f0
sstaley23 <sstaley23@gmail.com>
2023-06-22 11:33:31 -0600

    Created getCustomerAppointments method

--- src/DB/AppointmentsDB.java
+++ src/DB/AppointmentsDB.java
@@ -235,4 +235,41 @@ public abstract class AppointmentsDB {
         }
         return count;
     }
+
+    /** Retrieves a list of all appointments for a given customer
+     * @param custID
+     * @return
+     * @throws SQLException
+     */
+    public static ObservableList<Appointments> getCustomerAppointments(int custID) throws SQLException {
+        filteredAppointments.clear();
+        LocalDateTime startDateTime;
+        LocalDateTime endDateTime;
+
+        String sql = "SELECT appointments.Appointment_ID, appointments.Title, appointments.Description, appointments.Location, contacts.Contact_Name, appointments.Type, appointments.Start, appointments.End, appointments.Customer_ID, appointments.User_ID " +
+        "FROM appointments, contacts " +
+        "WHERE appointments.Contact_ID = contacts.Contact_ID AND appointments.Customer_ID = ?";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ps.setInt(1, custID);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            Appointments appointments = new Appointments(
+                    rs.getInt("Appointment_ID"),
+                    rs.getString("Title"),
+                    rs.getString("Description"),
+                    rs.getString("Location"),
+                    rs.getString("Contact_Name"),
+                    rs.getString("Type"),
+                    startDateTime = rs.getTimestamp("Start").toLocalDateTime(),
+                    startDateTime.toLocalDate(),
+                    startDateTime.toLocalTime(),
+                    endDateTime = rs.getTimestamp("End").toLocalDateTime(),
+                    endDateTime.toLocalDate(),
+                    endDateTime.toLocalTime(),
+                    rs.getInt("Customer_ID"),
+                    rs.getInt("User_ID"));
+            filteredAppointments.add(appointments);
+        }
+        return filteredAppointments;
+    }
 }
