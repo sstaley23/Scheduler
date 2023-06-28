d6a673c9fdd914cf7572e37f0a82d7f118ad7506
sstaley23 <sstaley23@gmail.com>
2023-06-28 10:17:31 -0600

    Created onMain to nav back to main menu

--- src/Controller/ReportsMenu.java
+++ src/Controller/ReportsMenu.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ReportsMenu {
+
+    Stage stage;
+    Parent scene;
+
+    public void onMain(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View/MainMenu.fxml"));
+        stage.setTitle("Main Menu");
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
 }
