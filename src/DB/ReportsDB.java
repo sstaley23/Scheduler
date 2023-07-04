d7ae21e01fa5cad34dbbdc86c82453fae6f98220
sstaley23 <sstaley23@gmail.com>
2023-07-03 21:51:20 -0600

    Created getMonthsForYear and filteredMonthTypesAppts methods

--- src/DB/ReportsDB.java
+++ src/DB/ReportsDB.java
@@ -1,6 +1,8 @@
 package DB;
 
-import Model.repMonthType;
+import Model.Month;
+import Model.MonthType;
+import Model.TypeCount;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -15,9 +17,9 @@ public abstract class ReportsDB {
      * @return
      * @throws SQLException
      */
-    public static ObservableList<repMonthType> getMonthTypeAppts() throws SQLException {
+    public static ObservableList<MonthType> getMonthTypeAppts() throws SQLException {
 
-        ObservableList<repMonthType> monthTypeAppts = FXCollections.observableArrayList();
+        ObservableList<MonthType> monthTypeAppts = FXCollections.observableArrayList();
 
         String sql = "SELECT Year(Start) as Year, monthname(Start) as Month, Type, count(*) as Count "  +
                 "FROM appointments " +
@@ -25,7 +27,7 @@ public abstract class ReportsDB {
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
         while (rs.next()) {
-            repMonthType query = new repMonthType(
+            MonthType query = new MonthType(
                     rs.getString("Year"),
                     rs.getString("Month"),
                     rs.getString("Type"),
@@ -34,4 +36,53 @@ public abstract class ReportsDB {
         }
         return monthTypeAppts;
     }
+
+    /** Generates a list of Months by year
+     * @param year
+     * @return
+     * @throws SQLException
+     */
+    public static ObservableList<Month> getMonthsForYear(String year) throws SQLException {
+
+        ObservableList<Month> months = FXCollections.observableArrayList();
+
+        String sql = "SELECT distinct monthname(start) as month FROM appointments WHERE year(Start) = ?";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ps.setString(1, year);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()){
+            Month query = new Month(
+                    rs.getString("Month"));
+            months.add(query);
+        }
+        return months;
+    }
+
+    /** Generates a list of types and counts of types for a given month and year
+     * @param year
+     * @param month
+     * @return
+     * @throws SQLException
+     */
+    public static ObservableList<TypeCount> filteredMonthTypeAppts(String year, String month) throws SQLException {
+
+        ObservableList<TypeCount> list = FXCollections.observableArrayList();
+
+        String sql = "SELECT DISTINCT Type, COUNT(Type) as Count " +
+                "FROM appointments " +
+                "WHERE YEAR(Start) = ? AND MONTHNAME(Start) = ? " +
+                "GROUP BY Type";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ps.setString(1, year);
+        ps.setString(2, month);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            TypeCount query = new TypeCount(
+                    rs.getString("Type"),
+                    rs.getInt("Count"));
+            list.add(query);
+        }
+        return list;
+    }
+
 }
