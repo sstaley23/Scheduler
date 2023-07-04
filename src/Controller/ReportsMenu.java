cdfe272e6aaaac4a63e9fd0a88f4f1334230d357
sstaley23 <sstaley23@gmail.com>
2023-07-03 21:53:56 -0600

    Added below methods for the generation of the Appts. by month type report -onClickMonth -genATCTable -atcOnSelect

--- src/Controller/ReportsMenu.java
+++ src/Controller/ReportsMenu.java
@@ -1,7 +1,8 @@
 package Controller;
 
 import DB.ReportsDB;
-import Model.repMonthType;
+import Model.MonthType;
+import Model.TypeCount;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,8 +11,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,11 +23,24 @@ import java.util.ResourceBundle;
 
 public class ReportsMenu implements Initializable {
 
+
+
     Stage stage;
     Parent scene;
 
     @FXML
     public ComboBox atcComboYear;
+    @FXML
+    public Label txtDialogue;
+    @FXML
+    public ComboBox atcComboMonth;
+    @FXML
+    public TableView<TypeCount> atcTableView;
+    @FXML
+    public TableColumn<TypeCount, String> atcColType;
+    @FXML
+    public TableColumn<TypeCount, Integer> atcColCount;
+
 
     /**Navigates back to the main menu
      * @param actionEvent
@@ -46,12 +61,54 @@ public class ReportsMenu implements Initializable {
     public ObservableList genyear() throws SQLException {
         ObservableList<String> year = FXCollections.observableArrayList();
 
-        for(repMonthType r : ReportsDB.getMonthTypeAppts()){
+        for(MonthType r : ReportsDB.getMonthTypeAppts()){
             if(!year.contains(r.getYear())){
                 year.add(r.getYear());
             }
         }
         return year;
+    }
+
+    /**Generates a list of months after a year is selected
+     * @param mouseEvent
+     * @throws SQLException
+     */
+    public void onClickMonth(MouseEvent mouseEvent) throws SQLException {
+        if(atcComboYear.getSelectionModel().isEmpty()){
+            txtDialogue.setText("Please select a year first...");
+        }else{
+            String year = atcComboYear.getValue().toString();
+            atcComboMonth.setItems(ReportsDB.getMonthsForYear(year));
+        }
+    }
+
+    /**Generates the table view for the ATC table
+     * @param list
+     */
+    public void genATCTable(ObservableList<TypeCount> list){
+        atcTableView.setItems(list);
+        atcColType.setCellValueFactory(new PropertyValueFactory<>("type"));
+        atcColCount.setCellValueFactory(new PropertyValueFactory<>("count"));
+    }
+
+    /**Verifies that the year/month combos are not empty and calls genATCTable if not
+     * @param actionEvent
+     * @throws SQLException
+     */
+    public void atcOnSelect(ActionEvent actionEvent) throws SQLException {
+        Boolean ifYear = atcComboYear.getSelectionModel().isEmpty();
+        Boolean ifMonth = atcComboMonth.getSelectionModel().isEmpty();
+
+        if(ifYear || ifMonth){
+            txtDialogue.setText("Please select values.");
+        }else {
+            txtDialogue.setText("");
+
+            String year = atcComboYear.getValue().toString();
+            String month = atcComboMonth.getValue().toString();
+
+            genATCTable(ReportsDB.filteredMonthTypeAppts(year, month));
+        }
     }
 
     @Override
@@ -62,4 +119,5 @@ public class ReportsMenu implements Initializable {
             throwables.printStackTrace();
         }
     }
+
 }
