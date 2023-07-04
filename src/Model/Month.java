24cf3b7f357a6fd034050e904b85cfb497b3c88d
sstaley23 <sstaley23@gmail.com>
2023-07-03 21:48:57 -0600

    Initial Commit

---
+++ src/Model/Month.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class Month {
+    String month;
+
+    public Month(String month) {
+        this.month = month;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    @Override
+    public String toString() {return(month);}
+}
---
+++ src/Model/TypeCount.java
@@ -0,0 +1,28 @@
+package Model;
+
+public class TypeCount {
+
+    private String type;
+    private int count;
+
+    public TypeCount(String type, int count) {
+        this.type = type;
+        this.count = count;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
